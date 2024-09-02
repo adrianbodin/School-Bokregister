56658af9664047848845583728228361431e7688
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-09-02 13:33:29 +0200

    Added seed data for the books

--- src/api/Data/ApplicationDbContext.cs
+++ src/api/Data/ApplicationDbContext.cs
@@ -9,4 +9,22 @@ public class ApplicationDbContext : DbContext
 
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        SeedBooks(modelBuilder);
+    }
+
+    private void SeedBooks(ModelBuilder builder)
+    {
+        builder.Entity<Book>().HasData([
+            new Book{ Isbn = "9780061120084", Title = "To Kill a Mockingbird", Author = "Harper Lee" },
+            new Book{Isbn = "9780451524935", Title = "1984", Author = "George Orwell"},
+            new Book{Isbn = "9780743273565", Title = "The Great Gatsby", Author = "F. Scott Fitzgerald"},
+            new Book{Isbn = "9781503290563", Title = "Pride and Prejudice", Author = "Jane Austen"},
+            new Book{Isbn = "9780316769488",Title = "The Catcher in the Rye",Author = "J.D. Salinger"}
+        ]);
+    }
 }
