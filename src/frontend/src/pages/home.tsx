1c4887e9ba9380a96eb05537011bd7937c0ed69e
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-09-03 11:21:02 +0200

    Added sorting and searching for the book list

--- src/frontend/src/pages/home.tsx
+++ src/frontend/src/pages/home.tsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from "react";
 import {Book} from "../types/book.ts";
 import '../css/home.css'
 import {Link} from "react-router-dom";
-import {deleteBook, getAllBooks} from "../fetching/books.ts";
+import { getAllBooks, SortBy} from "../fetching/books.ts";
 import AddBookForm from "../components/AddBookForm.tsx";
 
 const Home = () => {
@@ -20,12 +20,22 @@ const Home = () => {
     setBooks((prevBooks) => [...prevBooks, newBook]);
   };
 
-  const removeBook = (isbn: string) => async () => {
-    const answer = confirm("Are you sure you want to delete the book?");
+  const sortBooks = async (value: SortBy) => {
+    if (value) {
+      const sortedBooks = await getAllBooks(value);
+      setBooks(sortedBooks);
+    }
+  };
 
-    if(answer){
-      await deleteBook(isbn);
-      setBooks((prevBooks) => prevBooks.filter(book => book.isbn !== isbn));
+  //This is not optimal, it hits the api very many times, would need a look at.
+  const filterBooks = async (value: string) => {
+    if (value) {
+      const filteredBooks = await getAllBooks(undefined, value);
+      setBooks(filteredBooks);
+    }
+    else{
+      const books = await getAllBooks();
+      setBooks(books)
     }
   };
 
@@ -33,13 +43,23 @@ const Home = () => {
     <section className="book-section">
       <div>
         <h1>Alla böcker</h1>
+        <div className="filter-container">
+          <select onChange={(e) => sortBooks(e.target.value as SortBy)} name="sort" id="sort-select">
+            <option value="">Sortera efter:</option>
+            <option value="title">Titel</option>
+            <option value="author">Författare</option>
+          </select>
+          <search className="search-group">
+            <label htmlFor="search-input">Sök:</label>
+            <input onChange={(e) => filterBooks(e.target.value)} name="search-input" id="search-input" type="text" placeholder="Sökterm"/>
+          </search>
+        </div>
         <table>
           <thead className="table-head">
           <tr>
             <th scope="col">Boktitel</th>
             <th scope="col">Författare</th>
             <th scope="col">ISBN</th>
-            <th scope="col"></th>
           </tr>
           </thead>
           <tbody className="table-body">
@@ -53,7 +73,6 @@ const Home = () => {
               </td>
               <td>{book.author}</td>
               <td>{book.isbn}</td>
-              <td><button onClick={removeBook(book.isbn)}>Ta bort</button></td>
             </tr>
           ))}
           </tbody>
