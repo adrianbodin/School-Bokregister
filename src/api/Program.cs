9cd58a9fa85454525682f12a70d280ea45c3d4c8
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2024-09-03 10:43:02 +0200

    Added policies to allow calling api from javascript

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -16,6 +16,9 @@ builder.Services.AddCors(options =>
         policy =>
         {
             policy.WithOrigins("http://localhost:5174", "http://localhost:5173");
+            policy.AllowAnyHeader();
+            policy.AllowCredentials();
+            policy.AllowAnyMethod();
         });
 });
 
